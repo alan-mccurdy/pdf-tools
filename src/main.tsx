a8cf12ef8c6cea434c9225b47482efa2d4c8be85
Alan <alan@pdf-tools.dev>
2026-09-08 19:52:46 -0600

    fix: SPA basename + 404 redirect preserve /pdf-tools/ prefix

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import './styles/globals.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HelmetProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/pdf-tools">
         <App />
       </BrowserRouter>
     </HelmetProvider>
